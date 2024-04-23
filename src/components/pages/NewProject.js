84538e7dfcb15dc5f7c17ba7fe1d743d07de6802
luiz <luizricardobarros@gmail.com>
2024-04-23 19:50:52 -0300

    Inicio formulario criar projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from './NewProject.module.css'
+import ProjectForm from '../project/ProjectForm'
 
 function NewProject() {
   return (
-    <div>NewProject</div>
+    <div className={styles.newproject_container}>
+      <h1>Criar Projeto</h1>
+      <p>Crie seu projeto para depois adicionar os serviços</p>
+      <ProjectForm />
+    </div>
   )
 }
 
---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function ProjectForm() {
+  return (
+    <form>
+      <div>
+        <input type="text" placeholder="insira o nome do pojeto" />
+      </div>
+      <div>
+        <input type="number" placeholder="Insira o orçamento total" />
+      </div>
+      <div>
+        <select name="category_id">
+          <option disabled selected>Selecione a categoria</option>
+        </select>
+      </div>
+      <div>
+        <input type="submit" value="Criar Projeto"/>
+      </div>
+    </form>
+  );
+}
+
+export default ProjectForm;
