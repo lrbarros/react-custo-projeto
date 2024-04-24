6063b47fb38b85c156da6535d374225dca55ba8e
luiz <luizricardobarros@gmail.com>
2024-04-24 13:53:56 -0300

    "Adiciona novo formulário de projeto com seleção de categoria, gerencia o envio de formulários e redireciona em caso de sucesso"
    Este diff adiciona um novo formulário para criação de projetos, incluindo seleção de categorias usando o componente Select. Também inclui o tratamento do envio do formulário, envio de um POST

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import styles from './NewProject.module.css'
-import ProjectForm from '../project/ProjectForm'
+import React from "react";
+import styles from "./NewProject.module.css";
+import ProjectForm from "../project/ProjectForm";
+import { useNavigate } from "react-router-dom";
 
 function NewProject() {
+  const navigate = useNavigate();
+
+  function createPost(project) {
+    //init costs and services
+    project.cost = 0;
+    project.services = [];
+   
+    fetch("http://localhost:5000/projects", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json)
+      .then(
+        (data) => {
+          console.log(data);
+          //redirect
+          navigate("/projects", {replace:true, state:{ message: "Projeto criado com sucesso!" }});
+        }
+      )
+      .catch((err) => console.log(err));
+  }
+
   return (
-    <div className={styles.newproject_container}>
-      <h1>Criar Projeto</h1>
-      <p>Crie seu projeto para depois adicionar os serviços</p>
-      <ProjectForm  btnText="Criar Projeto"/>
-    </div>
-  )
+          <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços</p>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto" />
+          </div>
+        );
 }
 
-export default NewProject
+export default NewProject;
